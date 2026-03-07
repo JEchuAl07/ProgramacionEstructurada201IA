bf8594e5c991769fd59a2eb778f53577b5c198fa
Jesús <jechual0704@gmail.com>
2026-03-06 23:56:32 -0600

    Ejercicio_faker

---
+++ src/ux.com.curso.programacion/unidad1/Faker/generador_datos.py
@@ -0,0 +1,8 @@
+#importar la librería de faker
+from faker import Faker
+faker = Faker('es_MX')
+
+print(f'nombre: {faker.name()}')
+print(f'direccion: {faker.address()}')
+print(f'email: {faker.email()}')
+print(f'numero de telefono: {faker.phone_number()}')
