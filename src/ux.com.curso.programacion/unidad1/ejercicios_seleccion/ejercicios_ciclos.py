fb2fb1abfd177a7b5d4a4ba116b72a67d8fdca71
Jesús <jechual0704@gmail.com>
2026-03-17 08:24:51 -0600

    Ejercicio de ciclos

---
+++ src/ux.com.curso.programacion/unidad1/ejercicios_seleccion/ejercicios_ciclos.py
@@ -0,0 +1,56 @@
+#Ejemplo de repeticion 
+
+def ejemplo_for():
+    print("Estructura FOR")
+
+    frutas = ["manzana", "banana", "naranja"]
+
+    #For para iterar listas
+    for fruta in frutas:
+        print(fruta)
+
+    #For para iterar rangos
+    for i in range(1,5):
+        print(i)
+
+    #For para iterar rangos con pasos
+    for i in range(1,10,2):
+        print(i)
+
+#Ejemplo de while
+def ejemplo_while():
+    print("Estructura WHILE")
+
+    contador = 0
+
+    while contador <= 5:
+        print(contador)
+        contador += 1
+
+#Ejemplo de Do While
+def ejemplo_do_while():
+    print("Estructura DO WHILE")
+
+    secreto = "python12"
+    intentos = 0
+
+    while True:
+        intentos_usuario = "python12" #simulamos la entrada del usuario
+        intentos += 1
+
+        if intentos_usuario == secreto:
+            print("¡Acceso concedido!")
+        else:
+            print("Contraseña incorrecta. Intenta de nuevo.")
+            break
+        print("\n")
+
+def main():
+    ejemplo_for()
+    print("\n")
+    ejemplo_while()
+    print("\n")
+    ejemplo_do_while()
+
+if __name__ == "__main__":
+    main()
