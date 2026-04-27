bf9ba249bd06001af85b63687d2f0d92b94ddadf
Jesús <jechual0704@gmail.com>
2026-04-27 07:42:18 -0600

    Ejercicio de agente logica

--- src/ux.com.curso.programacion/unidad3/discord/practica_01/agente_logica.py
+++ src/ux.com.curso.programacion/unidad3/discord/practica_01/agente_logica.py
@@ -13,13 +13,26 @@ def procesar_pregunta(mensaje_usuario):
     conocimientos = {
     #conceptos de estructura de control
         "if": "La sentencia if es un condicional. permite que el programa tome decisiones basandose en una condicion boolenana",
-    
+        "for": "El bucle for se utiliza para iterar sobre una secuencia (como una lista o un rango) y ejecutar un bloque de codigo varias veces",
+        "while": "El bucle while se ejecuta mientras una condicion sea verdadera. es util para repetir un bloque de codigo hasta que se cumpla una condicion",
+        "while not": "El bucle while not se ejecuta mientras una condicion sea falsa. es util para repetir un bloque de codigo hasta que se cumpla una condicion",
     #Tipos de datos
         "int": "Representa numeros enteros, como 1, 2, 3, no tiene parte decimal",
+        "float": "Representa numeros con parte decimal, como 3.14 o 0.001",
+        "str": "Representa cadenas de texto, como 'hola' o 'programacion'",
+        "bool": "Representa valores de verdad, como True o False",
     #Funciones y modularidad
         "def": "Es la palabra reservada para definir una funcion en python",
+        "return": "Se utiliza para devolver un valor desde una funcion",
+        "import": "Se utiliza para importar modulos o librerias en python",
+        "lambda": "Es una funcion anonima, es decir, una funcion sin nombre que se define en una sola linea",
     #operadores y sintaxis
         "print": "Es una funcion incorporada en python que se utiliza para mostrar informacion en la consola",
+        "=": "Es el operador de asignacion, se utiliza para asignar un valor a una variable",
+        "==": "Es el operador de igualdad, se utiliza para comparar dos valores y devuelve True si son iguales y False si no lo son",
+        "+": "Es el operador de suma, se utiliza para sumar numeros o concatenar cadenas de texto",
+        "-": "Es el operador de resta, se utiliza para restar numeros",
+        "*": "Es el operador de multiplicacion, se utiliza para multiplicar numeros",
     }
     #3.- Logica de busqueda
     for clave in conocimientos:
