3e25ec5162fba05dec481d4718ba786c4a31f281
Jesús <jechual0704@gmail.com>
2026-05-26 08:24:06 -0600

    Ejercicio de division

---
+++ src/ux.com.curso.programacion/unidad5/division.py
@@ -0,0 +1,11 @@
+def division (a, b):
+    print(a//b)
+    print(a/b)
+
+def main():
+    a = int(input("Ingrese el primer número: "))
+    b = int(input("Ingrese el segundo número: "))
+    division(a, b)
+
+if __name__ == "__main__":
+    main()
